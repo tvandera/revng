62b5c865c64cdfa53454eb2d49ae7536d13a437a
Antonio Frighetto <me@antoniofrighetto.com>
2020-11-12 09:58:55 +0100

    Introduce StackAnalysis2

--- include/revng/StackAnalysis/StackAnalysis.h
+++ include/revng/StackAnalysis/StackAnalysis.h
@@ -4,14 +4,28 @@
 // This file is distributed under the MIT License. See LICENSE.md for details.
 //
 
+#include <algorithm>
+#include <optional>
 #include <string>
 
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/SmallSet.h"
+#include "llvm/IR/BasicBlock.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/DOTGraphTraits.h"
+#include "llvm/Support/GraphWriter.h"
 
+#include "revng/ADT/GenericGraph.h"
 #include "revng/BasicAnalyses/GeneratedCodeBasicInfo.h"
 #include "revng/FunctionCallIdentification/FunctionCallIdentification.h"
+#include "revng/Model/Binary.h"
 #include "revng/Model/LoadModelPass.h"
+#include "revng/StackAnalysis/AAWriterPass.h"
 #include "revng/StackAnalysis/FunctionsSummary.h"
+#include "revng/StackAnalysis/IndirectBranchInfoPrinterPass.h"
+#include "revng/StackAnalysis/PromoteGlobalToLocalVars.h"
+#include "revng/StackAnalysis/SegregateDirectStackAccesses.h"
+#include "revng/Support/OpaqueFunctionsPool.h"
 
 namespace StackAnalysis {
 
@@ -52,4 +66,171 @@ public:
   std::string TextRepresentation;
 };
 
+struct FunctionSummary {
+  FunctionSummary(
+    model::FunctionType::Values Type,
+    const std::set<llvm::GlobalVariable *> &ClobberedRegisters,
+    const llvm::SmallSet<
+      std::pair<llvm::CallInst *, model::FunctionEdgeType::Values>,
+      4> &Result,
+    std::optional<uint64_t> ElectedFSO,
+    llvm::Function *FakeFunction) :
+    Type(Type),
+    ClobberedRegisters(ClobberedRegisters),
+    Result(Result),
+    ElectedFSO(ElectedFSO),
+    FakeFunction(FakeFunction) {}
+
+  FunctionSummary() = delete;
+  FunctionSummary(const FunctionSummary &) = delete;
+  FunctionSummary(FunctionSummary &&) = default;
+  FunctionSummary &operator=(const FunctionSummary &) = delete;
+  FunctionSummary &operator=(FunctionSummary &&) = default;
+
+  model::FunctionType::Values Type;
+  std::set<llvm::GlobalVariable *> ClobberedRegisters;
+  llvm::SmallSet<std::pair<llvm::CallInst *, model::FunctionEdgeType::Values>,
+                 4>
+    Result;
+  std::optional<uint64_t> ElectedFSO;
+  llvm::Function *FakeFunction;
+
+  static bool compare(const FunctionSummary &Old, const FunctionSummary &New) {
+    if (New.Type == Old.Type)
+      return std::includes(Old.ClobberedRegisters.begin(),
+                           Old.ClobberedRegisters.end(),
+                           New.ClobberedRegisters.begin(),
+                           New.ClobberedRegisters.end());
+
+    return (New.Type <= Old.Type);
+  }
+
+  void dump() const debug_function { dump(dbg); }
+
+  template<typename T>
+  void dump(T &Output) const {
+    Output << "Dumping summary of the function. \n"
+           << "Type: " << Type << "\n"
+           << "ElectedFSO: " << *ElectedFSO << "\n"
+           << "Clobbered registers: ";
+
+    for (auto *Reg : ClobberedRegisters)
+      Output << ::getName(Reg);
+    Output << "\n";
+  }
+};
+
+class FunctionAnalysisResults {
+private:
+  /// \brief Map from CFEP to its function description
+  std::map<llvm::BasicBlock *, FunctionSummary> FunctionsBucket;
+  FunctionSummary DefaultSummary;
+
+  const FunctionSummary &get(llvm::BasicBlock *BB) const {
+    auto It = FunctionsBucket.find(BB);
+    if (It != FunctionsBucket.end())
+      return It->second;
+    return DefaultSummary;
+  }
+
+public:
+  FunctionAnalysisResults(FunctionSummary DefaultSummary) :
+    DefaultSummary(std::move(DefaultSummary)) {}
+
+  model::FunctionType::Values getFunctionType(llvm::BasicBlock *BB) const {
+    return get(BB).Type;
+  }
+
+  bool isFakeFunction(llvm::BasicBlock *BB) const {
+    return getFunctionType(BB) == model::FunctionType::Values::Fake;
+  }
+
+  llvm::Function *getFakeFunction(llvm::BasicBlock *BB) const {
+    return get(BB).FakeFunction;
+  }
+
+  const auto &getRegistersClobbered(llvm::BasicBlock *BB) const {
+    return get(BB).ClobberedRegisters;
+  }
+
+  std::optional<uint64_t> getElectedFSO(llvm::BasicBlock *BB) const {
+    return get(BB).ElectedFSO;
+  }
+
+  bool registerFunction(llvm::BasicBlock *BB, FunctionSummary &&F) {
+    auto It = FunctionsBucket.find(BB);
+    if (It != FunctionsBucket.end()) {
+      bool Changed = FunctionSummary::compare(It->second, F);
+      It->second = std::move(F);
+      return !(Changed);
+    }
+    FunctionsBucket.try_emplace(BB, std::move(F));
+    return true;
+  }
+};
+
+template<class FunctionOracle>
+class CFEPAnalyzer {
+  llvm::Module &M;
+  llvm::LLVMContext &Context;
+  GeneratedCodeBasicInfo *GCBI;
+  FunctionOracle &Oracle;
+  llvm::Function *PreHookMarker;
+  llvm::Function *PostHookMarker;
+  llvm::Function *IndirectBranchInfoMarker;
+  OpaqueFunctionsPool<llvm::StringRef> OFPRegistersClobbered;
+
+public:
+  CFEPAnalyzer(llvm::Module &M,
+               GeneratedCodeBasicInfo *GCBI,
+               FunctionOracle &Oracle,
+               llvm::Function *PreHookMarker,
+               llvm::Function *PostHookMarker) :
+    M(M),
+    Context(M.getContext()),
+    GCBI(GCBI),
+    Oracle(Oracle),
+    PreHookMarker(PreHookMarker),
+    PostHookMarker(PostHookMarker),
+    OFPRegistersClobbered(&M, false) {}
+
+public:
+  FunctionSummary
+  analyze(const std::vector<llvm::GlobalVariable *> &, llvm::BasicBlock *BB);
+
+private:
+  llvm::Function *createDisposableFunction(llvm::BasicBlock *BB);
+  llvm::BasicBlock *integrateFunctionCallee(llvm::BasicBlock *BB);
+  void throwDisposableFunction(llvm::Function *F);
+  FunctionSummary
+  milkResults(const std::vector<llvm::GlobalVariable *> &, llvm::Function *F);
+};
+
 } // namespace StackAnalysis
+
+struct BasicBlockNodeData {
+  BasicBlockNodeData(llvm::BasicBlock *BB) : BB(BB){};
+  llvm::BasicBlock *BB;
+};
+using BasicBlockNode = BidirectionalNode<BasicBlockNodeData>;
+using SmallCallGraph = GenericGraph<BasicBlockNode>;
+
+template<>
+struct llvm::DOTGraphTraits<SmallCallGraph *>
+  : public llvm::DefaultDOTGraphTraits {
+  using EdgeIterator = llvm::GraphTraits<SmallCallGraph *>::ChildIteratorType;
+  DOTGraphTraits(bool IsSimple = false) : DefaultDOTGraphTraits(IsSimple) {}
+
+  static std::string
+  getNodeLabel(const BasicBlockNode *Node, const SmallCallGraph *Graph) {
+    if (Node->BB == nullptr)
+      return "null";
+    return Node->BB->getName().str();
+  }
+
+  static std::string getEdgeAttributes(const BasicBlockNode *Node,
+                                       const EdgeIterator EI,
+                                       const SmallCallGraph *Graph) {
+    return "color=black,style=dashed";
+  }
+};
